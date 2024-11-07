7bfad6b5f70ef1636c7757cef0166bd283433e90
Caio Vinícius <119364552+tobelikecaio@users.noreply.github.com>
2024-11-07 16:27:40 -0300

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,19 +1,60 @@
-const apiUrl = "https://sua-api.com/sensor"; // URL da API para dados de umidade e temperatura
-let dataInterval;
+let intervalo;
+let coletaAtiva = false;
 
-document.getElementById("startMonitoramento").addEventListener("click", () => {
-  if (dataInterval) return;
+function iniciarColeta() {
+  coletaAtiva = true;
 
-  dataInterval = setInterval(() => {
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        const sensorDataBox = document.getElementById("sensorData");
-        const output = `Temperatura: ${data.temperatura}°C, Umidade: ${data.umidade}%\n`;
-        sensorDataBox.value = output + sensorDataBox.value;
-      })
-      .catch((error) =>
-        console.error("Erro ao coletar dados do sensor:", error)
-      );
-  }, 1000);
-});
+  document.getElementById("startMonitoramento").disabled = true;
+  document.getElementById("stopMonitoramento").disabled = false;
+
+  intervalo = setInterval(trazerDados, 1000);
+}
+
+function pararColeta() {
+  coletaAtiva = false;
+
+  document.getElementById("startMonitoramento").disabled = false;
+  document.getElementById("stopMonitoramento").disabled = true;
+
+  clearInterval(intervalo);
+}
+
+function trazerDados() {
+  const apiUrl =
+    "https://ilh4nkogqj.execute-api.us-east-1.amazonaws.com/prod/CAIO-SENSOR";
+
+  const agora = new Date();
+  const hora = agora.getHours().toString().padStart(2, "0");
+  const minuto = agora.getMinutes().toString().padStart(2, "0");
+  const segundo = agora.getSeconds().toString().padStart(2, "0");
+  const tempo = `${hora}:${minuto}:${segundo}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      if (Array.isArray(data) && data.length > 0) {
+        const item = data[0];
+
+        if (item.umidade !== undefined && item.temperatura !== undefined) {
+          const newData = `Hora: ${tempo}\nUmidade: ${item.umidade}%\nTemperatura: ${item.temperatura}°C\n\n`;
+          document.getElementById("sensorData").value += newData;
+        } else {
+          document.getElementById(
+            "sensorData"
+          ).value += `Hora: ${tempo}\nDados não encontrados ou formato errado.\n\n`;
+        }
+      } else {
+        document.getElementById(
+          "sensorData"
+        ).value += `Hora: ${tempo}\nDados não encontrados ou formato errado.\n\n`;
+      }
+    })
+    .catch((error) => {
+      document.getElementById(
+        "sensorData"
+      ).value += `Hora: ${tempo}\nErro ao carregar dados.\n\n`;
+      console.error("Erro ao coletar dados:", error);
+    });
+}
